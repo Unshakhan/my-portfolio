febdb70c256efb07744d38eaf3e22039cc57a303
Unshakhan <unshak08@gmail.com>
2025-11-02 19:48:47 +0500

    update

---
+++ app.js
@@ -0,0 +1,70 @@
+
+// function submitForm(){
+//     var fname = document.getElementById("fname").value;
+//     var email = document.getElementById("email").value;
+//     var number = document.getElementById("number").value;
+//     var topic = document.getElementById("topic").value;
+//     var message = document.getElementById("msg").value;
+
+//     console.log(`Name: ${fname}
+// Email: ${email}
+// Phone: ${number}
+// Email subject: ${topic}
+// Message: ${message}
+// `);
+// }
+// _________________-
+// ___________OR
+// ________________
+// let name = "Unsha";
+// let age = 24;
+// let skills = ["HTML", "CSS", "JavaScript"];
+
+// console.log("Name: " + name);
+// console.log("Age: " + age);
+// console.log("Skills: " + skills.join(", "));
+
+//  ______________________________
+// function to add two numbers
+// _______________________________
+// function Add(a,b){
+// console.log(a + b);
+// } 
+// Add(2,4)
+// _________________OR
+// var num1 = Number(prompt("Enter your first number"))
+// var num2 = Number(prompt("Enter your second number"))
+// function Add(a,b){
+// console.log(`Your first number is : ${a}
+//     Your second number is : ${b}
+//     The Addition of both is : ${+a + +b}`);
+// }
+// Add(num1,num2);
+// ________________________________
+// find the average with variable only
+// ______________________________________
+// var Sub1= prompt("Enter your first Subject")
+// var Marks1 = +prompt("Enter your marks in " + Sub1)
+// var Sub2= prompt("Enter your second Subject")
+// var Marks2 = +prompt("Enter your marks in " + Sub2)
+// var Sub3= prompt("Enter your third Subject")
+// var Marks3 = +prompt("Enter your marks in " + Sub3)
+// var Average = (Marks1 + Marks2 +Marks3)/3
+// console.log(Average);
+// ________________________________
+// find the average with helpof function
+// ______________________________________
+// function Avg(a,b,c){
+// console.log((a+b+c)/3);
+// }
+// Avg(3,4,5)
+// ______________________
+// __________OR
+// ____________________________
+// function Avg(a,b,c){
+// console.log((a+b+c)/3);
+// }
+// var val1= Number(prompt("Enter first value"))
+// var val2= Number(prompt("Enter Second value"))
+// var val3= Number(prompt("Enter third value"))
+// Avg(val1,val2,val3)
