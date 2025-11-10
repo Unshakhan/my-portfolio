d944a0e6aff40b307f8372c3b23841eac1124fd8
Unshakhan <unshak08@gmail.com>
2025-11-10 20:54:45 +0500

    update

--- app.js
+++ app.js
@@ -76,40 +76,34 @@ function darkTheme(){
   var btn = document.getElementById("btn");
   var paras = hero.getElementsByTagName("p");
   var spans= hero.getElementsByTagName("span")
-//   var allh1 = hero.getElementsByTagName("h1")
-var myname=document.getElementsByClassName("MyNameheading")
+var myname=document.getElementById("MyNameheading")
 btn.style.backgroundColor = "transparent"
 btn.style.padding = "10px"
-// btn.style.transform ="scale(1.2)"
 btn.style.borderRadius = "10px"
-btn.style.position ="fixed"
-btn.style.bottom = "-700px";      // bottom se distance
-btn.style.right = "20px";  
-// btn.style.right = "0px";
-  if (hero.style.backgroundImage.includes("freepik.com")) {
+  btn.style.border ="2px solid #c9d6c2ff";
+    btn.style.color = "white";
+  if (hero.style.backgroundImage.includes("_hybrid&w=740&q=80")) {
     // btn.classList.remove("button");
     hero.style.backgroundImage = "url('./images/green-bg-2')";
     hero.style.backgroundSize = "contain";          
 hero.style.backgroundRepeat = "repeat";    
 // hero.style.backgroundPosition = "center"; 
     hero.style.color = "white";
-   btn.style.border ="2px solid #c9d6c2ff";
-    btn.style.color = "white";
+ 
     btn.innerHTML = "Light Mode";
     for (var i = 0; i < paras.length; i++) {
   paras[i].style.setProperty("color", "white", "important");
 }
   }
   else {
-  
-    hero.style.backgroundImage = "url('https://img.freepik.com/premium-vector/abstract-background-pastel-colors_627230-60.jpg?w=360')";
+    hero.style.backgroundImage = "linear-gradient(rgba(255, 255, 255, 0.14), rgba(255, 255, 255, 0.07)),url('https://img.freepik.com/free-vector/hand-painted-watercolor-nature-background_23-2148934719.jpg?semt=ais_hybrid&w=740&q=80')";
     hero.style.backgroundSize = "contain";         
 hero.style.backgroundRepeat = "repeat";  
 btn.innerHTML = "Dark Mode";
-btn.style.color = "black"
-// btn.classList.add("button");
-btn.style.border ="2px solid #192e0eff";
-myname[0].style.color = "green";
+myname.style.backgroundImage ="none"
+myname.style.backgroundColor ="green"
+myname.style.fontFamily ="MyPaintingFont"
+
 for (var i = 0; i < paras.length; i++) {
   paras[i].style.setProperty("color", "black", "important");
 }
@@ -117,7 +111,114 @@ for (var i = 0; i < paras.length; i++) {
 for (var i = 0; i < spans.length; i++) {
   spans[i].style.setProperty("color", "green", "important");
 }
+var parent = document.getElementsByClassName("cta-buttons")[0];
+var btns = parent.getElementsByTagName("a");
+
+for (var i = 0; i < btns.length; i++) {
+  btns[i].style.border = "3px solid green";
+  btns[i].style.color = "green";
+  btns[i].style.background = "none";
+}
 
   }
+
+}
+var buttons = document.getElementsByClassName("resume-btn");
+var details = document.getElementsByClassName("resume-detail");
+
+// SAB CONTENT HIDE KARNE KA FUNCTION
+function hideAll() {
+  for (var i = 0; i < details.length; i++) {
+    details[i].classList.remove("active");
+  }
+  for (var i = 0; i < buttons.length; i++) {
+    buttons[i].classList.remove("active");
+  }
+}
+
+// BUTTONS PAR EVENT LISTENER LAGANA
+for (let i = 0; i < buttons.length; i++) {
+  
+  buttons[i].addEventListener("click", function () {
+
+    hideAll(); // pehle sab hatao
+
+    // BUTTON ko active style
+    this.classList.add("active");
+
+    // index ke hisaab se correct content show karna
+    details[i].classList.add("active");
+  });
+}
+// ==================================== Dynamically Randered Skills Function 
+let mySkills = [
+  { name: "HTML", percent: 95 },
+  { name: "CSS", percent: 95 },
+  { name: "JavaScript", percent: 70 },
+  { name: "Bootstrap", percent: 90 },
+  
+];
+function renderSkills() {
+  const container = document.getElementById("skillsContainer");
+  container.innerHTML = "";
+
+  mySkills.forEach((skill, index) => {
+    container.innerHTML += `
+      <div class="box">
+        <div class="shadow"></div>
+        <div class="content">
+          <div class="percent" data-text="${skill.name}" style="--num: ${skill.percent}">
+            <div class="dot"></div>
+            <svg>
+              <circle cx="70" cy="70" r="70"></circle>
+              <circle cx="70" cy="70" r="70"></circle>
+            </svg>
+          </div>
+          <div class="number">
+            <h2>${skill.percent}<span>%</span></h2>
+          </div>
+
+          <button class="deleteBtn" onclick="removeSkill(${index})">Delete</button>
+        </div>
+      </div>
+    `;
+  });
+}
+function removeSkill(i) {
+  mySkills.splice(i, 1);
+  renderSkills();
+}
+document.getElementById("addSkillBtn").addEventListener("click", () => {
+  const name = document.getElementById("skillInput");
+  const percent = document.getElementById("skillPercent");
+
+  if (name.value.trim() !== "" && percent.value !== "") {
+    mySkills.push({
+      name: name.value,
+      percent: percent.value
+    });
+
+    name.value = "";
+    percent.value = "";
+
+    renderSkills();
+  }
+});
+ renderSkills();
+//  ================================Alert Function
+function showalert(e){
+var name = e.innerHTML
+
+console.log(name);
+Swal.fire({
+  title: "Hi Unsha!!!",
+  text: `You clicked on ${name}`,
+  customClass: {
+    popup: 'my-popup',
+    title: 'my-title',
+    htmlContainer: 'my-text',
+     confirmButton: 'my-btn'
+  }
+});
 
 }
